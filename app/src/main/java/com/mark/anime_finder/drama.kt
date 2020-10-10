a55f53edad39cdc056535a93af725ab00ca8d6e1
Mark Antao <35659231+markantao@users.noreply.github.com>
2020-10-10 13:02:49 -0400

    Added "March" page

--- app/src/main/java/com/mark/anime_finder/drama.kt
+++ app/src/main/java/com/mark/anime_finder/drama.kt
@@ -1,11 +1,19 @@
 package com.mark.anime_finder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class drama : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drama)
+
+        val marchad = findViewById<Button>(R.id.march_button)
+        marchad.setOnClickListener {
+            val intent = Intent(this, march::class.java)
+            startActivity(intent)
+        }
     }
 }
