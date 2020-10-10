b73e2376eb952822d36ac3aae5207a28d99964e2
Mark Antao <35659231+markantao@users.noreply.github.com>
2020-10-10 11:35:07 -0400

    Added new features

--- app/src/main/java/com/mark/anime_finder/comedy.kt
+++ app/src/main/java/com/mark/anime_finder/comedy.kt
@@ -1,11 +1,20 @@
 package com.mark.anime_finder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class comedy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comedy)
+
+        val samuraiad = findViewById<Button>(R.id.samurai_button)
+        samuraiad.setOnClickListener {
+            val intent = Intent(this, samurai::class.java)
+            startActivity(intent)
+        }
+
     }
 }
