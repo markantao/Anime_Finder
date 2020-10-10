43a8d775139586782bde19699b1203d0324eea10
Mark Antao <35659231+markantao@users.noreply.github.com>
2020-10-10 11:12:27 -0400

    Added new features

--- app/src/main/java/com/mark/anime_finder/adventure.kt
+++ app/src/main/java/com/mark/anime_finder/adventure.kt
@@ -1,11 +1,25 @@
 package com.mark.anime_finder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class adventure : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adventure)
+
+        val narutoad = findViewById<Button>(R.id.naruto_button)
+        narutoad.setOnClickListener {
+            val intent = Intent(this, naruto::class.java)
+            startActivity(intent)
+        }
+
+        val hunterad = findViewById<Button>(R.id.hunter_button)
+        hunterad.setOnClickListener {
+            val intent = Intent(this, hunter::class.java)
+            startActivity(intent)
+        }
     }
 }
